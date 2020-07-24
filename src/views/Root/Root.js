d4f63840327cacadec83b9bc173026421b03f077
Damian Dzialo <ddzialo28@interia.pl>
2020-07-24 12:45:51 +0200

    Remove tests

---
+++ src/views/Root/Root.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from 'components/Button/Button';
+
+const Root = () => (
+  <div>
+    <h1>Hello Damian</h1>;<Button>Enter</Button>
+  </div>
+);
+
+export default Root;
